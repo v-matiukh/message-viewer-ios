c702bff45ca0aaf484b834c45b5229cfc68a3b9c
Volodymyr Matiukh <vova.matiukh@gmail.com>
2024-09-19 10:42:13 +0300

    Initial Commit

---
+++ MessageViewer/MessageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageViewerApp.swift
+//  MessageViewer
+//
+//  Created by Volodymyr Matiukh on 19.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MessageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
